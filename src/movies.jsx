616949320d55d4c319b2574631aa6887f7f80452
khaled-codeur <manou-codeur@outlook.com>
2020-06-12 19:44:31 +0100

    start the movies app

---
+++ src/movies.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Movies extends Component {
+    render() { 
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+ 
+export default Movies;
