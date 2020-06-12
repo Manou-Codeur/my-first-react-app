a05b81cf27ac0fad539479d6810b50eb6a4b1000
khaled-codeur <manou-codeur@outlook.com>
2020-06-12 19:52:57 +0100

    movies data added

---
+++ src/movies-data.js
@@ -0,0 +1,39 @@
+const movies = [
+    {
+        _id: "5bfe5fz1szr6qsde7q3",
+        title: "Terminator",
+        genere: "Action",
+        numberInStock: 6,
+        dailyRentalRate: 2.5
+    },
+    {
+        _id: "5bfe5fz1szr2emde7q3",
+        title: "You",
+        genere: "Romance",
+        numberInStock: 7,
+        dailyRentalRate: 4.5
+    },
+    {
+        _id: "5bfe5g8hszr6qsde7q3",
+        title: "Dark",
+        genere: "Action",
+        numberInStock: 2,
+        dailyRentalRate: 4.7
+    },
+    {
+        _id: "5bfe5fz1sf0zqsde7q3",
+        title: "La Casa de Papel",
+        genere: "Police",
+        numberInStock: 25,
+        dailyRentalRate: 4.8
+    },
+    {
+        _id: "5bfe5foppzr6qsde7q3",
+        title: "Dynastie",
+        genere: "Daily",
+        numberInStock: 60,
+        dailyRentalRate: 4.1
+    }
+]
+
+export default movies;
--- src/movies.jsx
+++ src/movies.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import movies from './movies-data';
 class Movies extends Component {
     render() { 
         return (
             <div>
-                
+                <h1></h1>
             </div>
         )
     }
